fb9cda34a852c519b9f4917588b4c55346fb24bd
Shaun Brockhoff <shaun.brockhoff@gdit.com>
2016-04-01 22:19:43 -0500

    add patient call

--- src/test/java/org/ccwdata/web/service/FhirService2Test.java
+++ src/test/java/org/ccwdata/web/service/FhirService2Test.java
@@ -2,6 +2,7 @@ package org.ccwdata.web.service;
 
 import java.util.List;
 
+import org.ccwdata.web.pojo.PatientPojo;
 import org.hl7.fhir.dstu3.model.ExplanationOfBenefit;
 
 
@@ -11,6 +12,8 @@ public class FhirService2Test {
 
 	public static void main(String[] args) {
 		FhirService2 fhirService = new FhirService2();
+		
+		PatientPojo patient = fhirService.getPatientByPatientId("147462");
 //		147462
 		List<ExplanationOfBenefit> eobList = fhirService.getEobByPatientId("147462");
 		if(!eobList.isEmpty()) {
